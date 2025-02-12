bb0d5845bce1a4695c044ba94d57d5c835c7384a
AlpenmeisterCustoms <github@ezz.pw>
2025-02-12 20:44:00 +0100

    Update BoardConfig.h

--- configs/Alpenmeister/BoardConfig.h
+++ configs/Alpenmeister/BoardConfig.h
@@ -12,25 +12,32 @@
 #define BOARD_CONFIG_LABEL "Alpenmeister"
 
 // Main pin mapping Configuration
-//                                                  // GP2040 | Xinput | Switch  | PS3/4/5  | Dinput | Arcade |
-#define GPIO_PIN_02 GpioAction::BUTTON_PRESS_UP     // UP     | UP     | UP      | UP       | UP     | UP     |
-#define GPIO_PIN_03 GpioAction::BUTTON_PRESS_DOWN   // DOWN   | DOWN   | DOWN    | DOWN     | DOWN   | DOWN   |
-#define GPIO_PIN_04 GpioAction::BUTTON_PRESS_RIGHT  // RIGHT  | RIGHT  | RIGHT   | RIGHT    | RIGHT  | RIGHT  |
-#define GPIO_PIN_05 GpioAction::BUTTON_PRESS_LEFT   // LEFT   | LEFT   | LEFT    | LEFT     | LEFT   | LEFT   |
-#define GPIO_PIN_06 GpioAction::BUTTON_PRESS_B1     // B1     | A      | B       | Cross    | 2      | K1     |
-#define GPIO_PIN_07 GpioAction::BUTTON_PRESS_B2     // B2     | B      | A       | Circle   | 3      | K2     |
-#define GPIO_PIN_08 GpioAction::BUTTON_PRESS_R2     // R2     | RT     | ZR      | R2       | 8      | K3     |
-#define GPIO_PIN_09 GpioAction::BUTTON_PRESS_L2     // L2     | LT     | ZL      | L2       | 7      | K4     |
-#define GPIO_PIN_10 GpioAction::BUTTON_PRESS_B3     // B3     | X      | Y       | Square   | 1      | P1     |
-#define GPIO_PIN_11 GpioAction::BUTTON_PRESS_B4     // B4     | Y      | X       | Triangle | 4      | P2     |
-#define GPIO_PIN_12 GpioAction::BUTTON_PRESS_R1     // R1     | RB     | R       | R1       | 6      | P3     |
-#define GPIO_PIN_13 GpioAction::BUTTON_PRESS_L1     // L1     | LB     | L       | L1       | 5      | P4     |
-#define GPIO_PIN_00 GpioAction::BUTTON_PRESS_S1     // S1     | Back   | Minus   | Select   | 9      | Coin   |
-#define GPIO_PIN_01 GpioAction::BUTTON_PRESS_S2     // S2     | Start  | Plus    | Start    | 10     | Start  |
-#define GPIO_PIN_26 GpioAction::BUTTON_PRESS_L3     // L3     | LS     | LS      | L3       | 11     | LS     |
-#define GPIO_PIN_27 GpioAction::BUTTON_PRESS_R3     // R3     | RS     | RS      | R3       | 12     | RS     |
-#define GPIO_PIN_14 GpioAction::BUTTON_PRESS_A1     // A1     | Guide  | Home    | PS       | 13     | ~      |
-#define GPIO_PIN_15 GpioAction::BUTTON_PRESS_A2     // A2     | ~      | Capture | ~        | 14     | ~      |
+//                                                 
+#define GPIO_PIN_01 GpioAction::BUTTON_PRESS_S2 
+#define GPIO_PIN_02 GpioAction::BUTTON_PRESS_L2 
+#define GPIO_PIN_03 GpioAction::BUTTON_PRESS_UP
+#define GPIO_PIN_04 GpioAction::BUTTON_PRESS_L3
+#define GPIO_PIN_05 GpioAction::BUTTON_PRESS_L3
+#define GPIO_PIN_06 GpioAction::BUTTON_PRESS_Left 
+#define GPIO_PIN_07 GpioAction::BUTTON_PRESS_Left 
+#define GPIO_PIN_08 GpioAction::BUTTON_PRESS_Down 
+#define GPIO_PIN_09 GpioAction::BUTTON_PRESS_Down   
+#define GPIO_PIN_10 GpioAction::BUTTON_PRESS_Right 
+#define GPIO_PIN_11 GpioAction::BUTTON_PRESS_Right  
+#define GPIO_PIN_12 GpioAction::BUTTON_PRESS_L3   
+#define GPIO_PIN_13 GpioAction::BUTTON_PRESS_L1    
+#define GPIO_PIN_16 GpioAction::BUTTON_PRESS_A1   
+#define GPIO_PIN_17 GpioAction::BUTTON_PRESS_A2    
+#define GPIO_PIN_18 GpioAction::BUTTON_PRESS_S1   
+#define GPIO_PIN_19 GpioAction::BUTTON_PRESS_S2    
+#define GPIO_PIN_20 GpioAction::BUTTON_PRESS_B1  
+#define GPIO_PIN_21 GpioAction::BUTTON_PRESS_B3 
+#define GPIO_PIN_22 GpioAction::BUTTON_PRESS_B2 
+#define GPIO_PIN_23 GpioAction::BUTTON_PRESS_B4 
+#define GPIO_PIN_24 GpioAction::BUTTON_PRESS_R2 
+#define GPIO_PIN_25 GpioAction::BUTTON_PRESS_R1 
+#define GPIO_PIN_26 GpioAction::BUTTON_PRESS_R3 
+#define GPIO_PIN_27 GpioAction::BUTTON_PRESS_R3 
 
 // Keyboard Mapping Configuration
 //                                            // GP2040 | Xinput | Switch  | PS3/4/5  | Dinput | Arcade |
@@ -62,34 +69,34 @@
 #define LED_BRIGHTNESS_STEPS 5
 #define LED_FORMAT LED_FORMAT_GRB
 #define LEDS_PER_PIXEL 14
-#define LEDS_BASE_ANIMATION_INDEX 4
+#define LEDS_BASE_ANIMATION_INDEX 1
 #define LEDS_BUTTON_A2 1
 
 // Display Stuff
 //
 #define HAS_I2C_DISPLAY 1
-#define DISPLAY_I2C_BLOCK I2C1
 #define I2C1_ENABLED 1
-#define I2C1_PIN_SDA 14
-#define I2C1_PIN_SCL 15
+#define DISPLAY_I2C_BLOCK i2c1
+#define I2C_PIN_SDA 14
+#define I2C_PIN_SCL 15
 #define DISPLAY_FLIP 0
+#define SPLASH_MODE SPLASH_MODE_STATIC
+#define SPLASH_DURATION 3
 
 // USB Passthrough Stuff
 //
 #define USB_PERIPHERAL_ENABLED 1
 #define USB_PERIPHERAL_PIN_DPLUS 29
-//#define USB_PERIPHERAL_PIN_ORDER -1
+#define USB_PERIPHERAL_PIN_ORDER -1
 
-// Button Layout Stuff
+// Button Stuff
 //
 #define DEFAULT_SOCD_MODE SOCD_MODE_NEUTRAL
+#define DEFAULT_INPUT_MODE_B4 INPUT_MODE_PS5
+#define DEFAULT_PS5AUTHENTICATION_TYPE INPUT_MODE_AUTH_TYPE_USB
 
 #define BUTTON_LAYOUT BUTTON_LAYOUT_BOARD_DEFINED_A
 #define BUTTON_LAYOUT_RIGHT BUTTON_LAYOUT_BOARD_DEFINED_B
-#define SPLASH_MODE SPLASH_MODE_STATIC
-#define SPLASH_DURATION 3
-
-#define DEFAULT_INPUT_MODE_B4 INPUT_MODE_PS5
 
 #define DEFAULT_BOARD_LAYOUT_A {\
     {GP_ELEMENT_BTN_BUTTON, {7, 32, 7, 6, 1, 1, GAMEPAD_MASK_L2,       GP_SHAPE_ELLIPSE}},\
